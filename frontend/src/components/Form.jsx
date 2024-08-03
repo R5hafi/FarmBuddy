df98575d4aa4946fda0f564d13ebac50804b446b
Tobias2099 <tobiaspong@gmail.com>
2024-08-03 00:20:48 -0400

    add form inputs

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Form() {
+  return (
+    <>
+      <div class="form">
+        <label>N <input type="text" /></label>
+        <label>P <input type="text" /></label>
+        <label>K <input type="text" /></label>
+        <label>Temperature <input type="text" /></label>
+        <label>Humidity <input type="text" /></label>
+        <label>pH <input type="text" /></label>
+        <label>Rainfall <input type="text" /></label>
+      </div>
+    </>
+  )
+}
+
+export default Form;
