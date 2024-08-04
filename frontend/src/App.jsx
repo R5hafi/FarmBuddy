714dd9c08c9e5507bed4d56670f88988e7c29d5f
Ryan Shafi <76465122+R5hafi@users.noreply.github.com>
2024-08-04 05:05:11 -0400

    Updated App.jsx file
    Changed the icons/buttons in middle to the different images and plants from the dataset the machine learning algorithm was trained on.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Plant from "./components/Plant"
 import { useContext, useEffect } from 'react';
 import  { SelectedCellProvider } from './contexts/SelectedCellContext'; 
 import { CohereClient } from "cohere-ai";
+
 function App() {
   const [count, setCount] = useState(9) //size of grid
   const [suggestion, setSuggestion] = useState("");
@@ -50,10 +51,10 @@ function App() {
 
           <Form></Form>
           <div id="plant-list">
-            <Plant name="Sunflower" imageurl="https://www.gardendesign.com/pictures/images/675x529Max/site_3/ring-of-fire-sunflower-bicolor-sunflower-all-america-selections_12080.jpg"></Plant>
-            <Plant name="Soybeans" imageurl="https://cdn.britannica.com/28/154828-050-05C6239A/Soybeans.jpg"></Plant>
-            <Plant name="Lavender" imageurl="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Single_lavender_flower02.jpg/1200px-Single_lavender_flower02.jpg"></Plant>
-            <Plant name="Wheat" imageurl="https://www.foodnavigator.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/foodnavigator.com/news/science/wheat-yields-boosted-and-protein-content-increased-by-up-to-25-study/15414079-1-eng-GB/Wheat-yields-boosted-and-protein-content-increased-by-up-to-25-study.jpg"></Plant>
+            <Plant name="Rice" imageurl="https://www.apnikheti.com/upload/organic/1974idea99organic-rice1.jpg"></Plant>
+            <Plant name="Coffee" imageurl="https://safras.com.br/wp-content/uploads/2021/03/cafe-graos-23-1.jpg"></Plant>
+            <Plant name="Banana" imageurl="https://hartley-botanic.com/wp-content/uploads/2017/01/Image-1-Jan-2017.jpg"></Plant>
+            <Plant name="Cotton" imageurl="https://cdn.britannica.com/18/156618-050-39339EA2/cotton-harvesting.jpg"></Plant>
           </div>
           
             <GridView count={count} setCount={changeCount} modalText={suggestion === "" ? "Sample placeholder." : suggestion}></GridView>
