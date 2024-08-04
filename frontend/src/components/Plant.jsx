6d366824d4deac50e27d9be748b457e3dddc5075
Tobias2099 <tobiaspong@gmail.com>
2024-08-04 08:11:15 -0400

    plant buttons can be used to enter plant name in form

--- frontend/src/components/Plant.jsx
+++ frontend/src/components/Plant.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import '../styles/Plant.css'
 function Plant({name, imageurl}) {
+
+  function handleClick() {
+    document.getElementById("plant-name").value = name
+  }
   return (
     <>
-      <div className="plant-container">
+      <div onClick={() => {handleClick()}} className="plant-container">
         <span>{name}</span>
         <img src={imageurl} alt={"Picture of " + name} height="100px" width="100px"/>
       </div>  
